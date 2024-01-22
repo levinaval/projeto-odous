235b4463995feffefe4e3022d052ccc0f7d2c157
levi <levisantosluna@gmail.com>
2024-01-22 00:15:36 -0300

    Desenvolvendo estilização na página index

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -21,3 +21,28 @@ module.exports = {
     require('@tailwindcss/container-queries'),
   ]
 }
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'siano': '#083344',
+      'céu': '#082f49',
+      'Verde':'#22c55e'
+    },
+    extend: {
+      // ...
+    },
+  },
+  plugins: [],
+}
